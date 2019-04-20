eafbf88634aa4f969e4b469d77bb4dfbbc4557a8
ncristiansa <ncristiansalinasandia@gmail.com>
2019-04-20 15:15:21 +0200

    Boostrap añadido y tarea finalizada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
 <html lang="en">
     <head>
         <title>Laravel Quickstart - Basic</title>
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!-- CSS And JavaScript -->
     </head>
-
-    <body>
+    
+    <body style="margin:auto;width:60%;padding:5%;">
         <h2>TaskList - Laravel</h2>
         <div class="container">
             <nav class="navbar navbar-default">
